abe618132135f4722ff47325257812c4eae54db7
tahany777 <tahanymohammed777@hotmail.com>
2022-06-26 16:54:58 +0300

    feat: added sign in feature

--- auth/models/user.model.js
+++ auth/models/user.model.js
@@ -7,7 +7,7 @@ const Users = (sequelize, DataTypes) => sequelize.define('user', {
     username: {
         type: DataTypes.STRING,
         allowNull: false,
-        unique: true
+        // unique: true
     },
     password: {
         type: DataTypes.STRING,
